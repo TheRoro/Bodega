8f2732f591b24c7f2dd47e0cfe0b3f70389e1e1f
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2020-09-01 22:38:13 -0500

    Created main components and configured dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <b-navbar class="my-navbar" toggleable="sm" type="dark" variant="dark">
+        <b-navbar-brand href="#">
+          <img src="../src/assets/LogoMini.png" class="d-inline-block align-top nav-logo" alt="Wapo logo" >
+        </b-navbar-brand>
+        <b-navbar-brand href="#">EconoWapo</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">
+              <b-button variant="outline-primary" @click="$router.push('login')">Login</b-button>
+            </b-nav-item>
+            <b-nav-item href="#">
+              <b-button variant="outline-success" @click="$router.push('register')">Register</b-button>
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import './assets/scss/app.scss';
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login">
+    <div class="row justify-content-center">
+      <div class="col-12 col-sm-6">
+        <h1>Log In</h1>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+            <b-form-input
+              id="input-1"
+              v-model="form.email"
+              type="email"
+              required
+              placeholder="Enter email"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+            <b-form-input
+              id="input-2"
+              v-model="form.password"
+              required
+              placeholder="Enter Password"
+              type="password"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">LOGIN</b-button>
+        </b-form>
+        <b-card class="mt-3" header="Form Data Result">
+          <pre class="m-0">{{ form }}</pre>
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.password = ''
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
