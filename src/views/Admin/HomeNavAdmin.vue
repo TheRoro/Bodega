30049264c977aaa2af876d352d67cc0f38531ed7
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-10-23 13:52:53 -0500

    Admin NavBar

---
+++ src/views/Admin/HomeNavAdmin.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="HomeNav">
+    <b-navbar class="my-navbar" toggleable="sm" type="dark" variant="">
+    <b-navbar-brand to="/">
+        <img src="../../../public/assets/logo.jpeg" class="d-inline-block align-top nav-logo" alt="Wapo logo" >
+    </b-navbar-brand>
+    <b-navbar-brand to="/">FinanciApp</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+        <b-nav-item class="text-primary" to="/">Home</b-nav-item>
+        <b-nav-item to="/about">Contáctanos</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/allProducts">
+            <b-button size="lg" variant="primary text-success" class="nav-btn">Productos</b-button>
+        </b-nav-item>
+        <b-nav-item to="/orders">
+            <b-button size="lg" variant="primary text-success" class="nav-btn">Clientes</b-button>
+        </b-nav-item>
+        <b-nav-item to="/products">
+            <b-button size="lg" variant="primary text-success" class="nav-btn">Pagos</b-button>
+        </b-nav-item>
+        <b-nav-item to="/products">
+            <b-button size="lg" variant="primary text-success" class="nav-btn">Pedidos</b-button>
+        </b-nav-item>
+        <b-navbar-brand to="/profile">
+            <img v-if="this.$store.getters.profilePicture == ''" src="../../../public/assets/user.png" class="d-inline-block align-top nav-logo" alt="Wapo logo" >
+            <img v-if="this.$store.getters.profilePicture" :src="this.$store.getters.profilePicture"  class="d-inline-block align-top nav-logo"/>
+                        
+        </b-navbar-brand>
+        </b-navbar-nav>
+    </b-collapse>
+    </b-navbar>
+  </div>
+</template>
