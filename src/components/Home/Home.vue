1e33bb1b4d52fd093206232363b924274a30a902
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-01-02 01:02:33 -0500

    🔨 Refactored Home
    Refactored Home Component

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home container">
+    <div class="justify-content-center align-items-center row full-height">
+      <div class="col-12">
+        <h1>Realiza tus cuentas,</h1>
+        <h1>desde tu bodega</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../../assets/scss/app.scss';
+@import './Home.scss';
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+  },
+});
+</script>
