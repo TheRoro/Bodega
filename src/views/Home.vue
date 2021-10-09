f6c40140a866e1820dcdf8653cbf9bc59be7debe
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-10-09 18:47:33 -0500

    ✨Home View

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<HomeModule />
+</template>
+
+<script>
+import HomeModule from '../modules/home/Home.vue'
+
+export default {
+	name: 'Home',
+	components: {
+		HomeModule,
+	},
+}
+</script>
