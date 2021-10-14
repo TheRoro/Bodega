58e3a4bfe614b694623ebda0e9f9063bc6bfee4c
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-10-14 15:19:59 -0500

    ✨Responsive Navbar

---
+++ src/modules/nav/Nav.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="nav">
+		<div class="brand" @click="brandHome()">
+			<span class="material-icons logo">storefront</span>
+			<h1 class="title">BODEGA</h1>
+		</div>
+		<div class="links">
+			<router-link to="/store" class="router-link">Shop</router-link>
+			<router-link to="/cart" class="router-link">Cart</router-link>
+			<router-link to="/" class="router-link">Home</router-link>
+		</div>
+		<div class="profile">
+			<img
+				class="photo"
+				src="../../assets/img/user_photo.png"
+				alt="user photo"
+			/>
+		</div>
+		<div class="bottom-navbar">
+			<div class="responsive-links">
+				<router-link to="/store" class="router-link">Shop</router-link>
+				<router-link to="/cart" class="router-link">Cart</router-link>
+				<router-link to="/" class="router-link">Home</router-link>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+@import './Nav.scss';
+</style>
+
+<script>
+import router from '@/router'
+export default {
+	name: 'Nav',
+	methods: {
+		brandHome() {
+			router.push('/')
+		},
+	},
+}
+</script>
