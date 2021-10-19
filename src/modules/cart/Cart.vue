98e1fa12be4747e625c849341dc6ca80cb98e587
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-10-19 10:12:33 -0500

    ✨Cart View

---
+++ src/modules/cart/Cart.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="cart">
+		<div class="text-row">
+			<h1 class="title">Shopping Cart</h1>
+		</div>
+		<div class="items-list">
+			<Item
+				v-for="item in items"
+				:key="item.id"
+				:id="item.id"
+				:name="item.name"
+				:price="item.price"
+				:img-url="item.imgUrl"
+				:quantity="item.quantity"
+			/>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+@import './Cart.scss';
+</style>
+
+<script>
+import Item from '../../components/item/Item.vue'
+export default {
+	name: 'Cart',
+	components: {
+		Item,
+	},
+	mounted() {
+		this.items = this.$store.getters.cart
+	},
+	data: function() {
+		return {
+			items: [],
+		}
+	},
+}
+</script>
---
+++ src/views/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<Nav />
+		<Cart />
+	</div>
+</template>
+
+<script>
+import Nav from '../modules/nav/Nav.vue'
+import Cart from '../modules/cart/Cart.vue'
+
+export default {
+	name: 'CartView',
+	components: {
+		Nav,
+		Cart,
+	},
+}
+</script>
