1b863cacfc72e1eccedb609f79d6fc99c5ce764f
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-10-23 13:36:15 -0500

    🎉 Finish Shop Module

---
+++ src/components/loading/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="loading" v-if="loading">
+		<div class="lds-ring">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+@import './Loading.scss';
+</style>
+
+<script>
+export default {
+	name: 'Loading',
+	props: {
+		loading: Boolean,
+	},
+}
+</script>
