6179eb60aff521cdc37dfe98f9d42fc5cf5c502c
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-10-25 09:16:31 -0500

    ✨Checkout Component

---
+++ src/components/checkout/Checkout.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="checkout" v-if="display">
+		<div class="subtotal">
+			<p class="text">Subtotal:</p>
+			<p class="price">$ {{ formatPrice(subtotal) }}</p>
+		</div>
+		<button class="button">Check out</button>
+	</div>
+</template>
+
+<style lang="scss">
+@import './Checkout.scss';
+</style>
+
+<script>
+export default {
+	name: 'Checkout',
+	props: {
+		display: Boolean,
+	},
+	methods: {
+		formatPrice(value) {
+			const val = (value / 1).toFixed(2).replace('.', '.')
+			return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+		},
+		checkout() {
+			//checkout logic
+		},
+	},
+	computed: {
+		subtotal() {
+			return this.$store.getters.subtotal
+		},
+	},
+}
+</script>
