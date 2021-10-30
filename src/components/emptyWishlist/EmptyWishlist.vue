e62c44c0a07f09b534b00a0c0687d9247489d7c1
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-10-30 10:19:10 -0500

    ✨Empty Wishlist

---
+++ src/components/emptyWishlist/EmptyWishlist.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="empty-wishlist" v-if="isEmpty">
+		<img
+			class="empty-svg"
+			src="../../assets/img/shopping_cart.svg"
+			alt="empty cart logo"
+			v-if="index % 2 == 0"
+		/>
+		<img
+			class="empty-svg"
+			src="../../assets/img/empty_cart.svg"
+			alt="empty cart logo"
+			v-if="index % 2 != 0"
+		/>
+		<h2 class="title">Your wishlist is empty</h2>
+		<p class="subtitle">
+			Looks like you haven't added
+		</p>
+		<p class="subtitle">
+			anything to your wishlist yet
+		</p>
+		<button class="cta-button" @click="goToShop()">Explore our products</button>
+	</div>
+</template>
+
+<style lang="scss">
+@import './EmptyWishlist.scss';
+</style>
+
+<script>
+export default {
+	name: 'EmptyWishlist',
+	props: {
+		isEmpty: Boolean,
+	},
+	data: function() {
+		return {
+			index: 0,
+		}
+	},
+	mounted() {
+		this.index = Math.floor(Math.random() * (2 - 0) + 0)
+	},
+	methods: {
+		goToShop() {
+			this.$router.push('/shop')
+		},
+	},
+}
+</script>
