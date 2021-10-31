eb75df42fdca414e0966d647526129a1a0e83d28
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-10-31 10:05:02 -0500

    🎉 Wishlist Module

---
+++ src/modules/wishlist/Wishlist.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="wishlist">
+		<div class="text-row">
+			<h1 class="title">My wishlist</h1>
+		</div>
+		<div class="items-grid">
+			<Card
+				v-for="item in this.$store.getters.wishlist"
+				:key="item.id"
+				:id="item.id"
+				:name="item.title"
+				:price="item.price"
+				:img-url="item.image"
+				:is-favorite="item.isFavorite"
+				:rating="item.rating"
+			/>
+		</div>
+		<EmptyWishlist :isEmpty="this.$store.getters.wishlist.length == 0" />
+	</div>
+</template>
+
+<style lang="scss">
+@import './Wishlist.scss';
+</style>
+
+<script>
+import Card from '../../components/card/Card.vue'
+import EmptyWishlist from '../../components/emptyWishlist/EmptyWishlist.vue'
+export default {
+	name: 'Wishlist',
+	components: {
+		Card,
+		EmptyWishlist,
+	},
+}
+</script>
